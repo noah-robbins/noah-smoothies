21be443e70302ecf7cc805f4759ed4c26fd6529b
noah-robbins <168755353+noah-robbins@users.noreply.github.com>
2024-05-24 11:24:21 +0100

    Update streamlit_app.py
    added loc function on pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-
-## Streamlit's documentation to get sample code to copy and paste into our SiS app. 
-#option = st.selectbox(
- #   "Choose your favourite fruits",
-  #  ("Stawberry", "Banana", "Mango", "Peach", "Pineapple"))
-
 #st.write("You selected:", option)
 
 #choose just the fruits column of the table
@@ -32,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #convert snowpark dataframe to pandas Dataframe to use LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 #print the instructions
@@ -49,6 +43,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '     #adds the chosen fruit to the ingredients_string list
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data = fruityvice_response.json(),use_container_width= True)
